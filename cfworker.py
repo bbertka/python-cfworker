53219bcabc51392e9da55aefdf603faf89678fb2
Ben Bertka <bbertka@gmail.com>
2015-05-26 15:28:51 -0700

    updated for port

--- cfworker.py
+++ cfworker.py
@@ -4,11 +4,12 @@ from flask import Flask
 import threading
 
 class cfworker(threading.Thread):
-        def __init__(self, port):
+        def __init__(self, port=None):
                 threading.Thread.__init__(self)
                 self.start()
+		self.port = port
        	        self.app = Flask(__name__)
-               	self.app.run( host='0.0.0.0', port=port )
+               	self.app.run( host='0.0.0.0', port=self.port )
 
         def run(self):
                 self.work()
