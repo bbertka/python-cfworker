c0e372432cdd96a6a597b79cdd1ab88d9bbbc8f7
Ben Bertka <bbertka@gmail.com>
2015-05-26 16:17:56 -0700

    testing

--- cfworker.py
+++ cfworker.py
@@ -3,13 +3,14 @@ import os
 from flask import Flask
 import threading
 
+app = Flask(__name__)
+
 class cfworker(threading.Thread):
         def __init__(self, port=None):
                 threading.Thread.__init__(self)
                 self.start()
 		self.port = port
-       	        self.app = Flask(__name__)
-               	self.app.run( host='0.0.0.0', port=self.port )
+               	app.run( host='0.0.0.0', port=self.port )
 
         def run(self):
                 self.work()
@@ -17,3 +18,8 @@ class cfworker(threading.Thread):
         def work(self):
                 # implement this function in your own app    
                 pass
+
+@app.route('/')
+def index():
+        return "Welcome to cfworker"
+
