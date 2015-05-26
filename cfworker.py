48f0140ffddaf0d53b0cef34a85ebbafee5a1a74
Ben Bertka <bbertka@gmail.com>
2015-05-26 16:39:32 -0700

    added logging

--- cfworker.py
+++ cfworker.py
@@ -2,14 +2,17 @@
 import os
 from flask import Flask
 import threading
+import logging
 
 app = Flask(__name__)
 
 class cfworker(threading.Thread):
-        def __init__(self, port=None):
+        def __init__(self, port=None, logger=None):
                 threading.Thread.__init__(self)
-                self.start()
 		self.port = port
+		if logger:
+			logging.getLogger('werkzeug').setLevel(logger)
+		self.start()
                	app.run( host='0.0.0.0', port=self.port )
 
         def run(self):
