1589671f9eaf206614e7e2110278c020411d2a17
Ben Bertka <bbertka@gmail.com>
2015-05-26 17:08:09 -0700

    added default CF port

--- cfworker.py
+++ cfworker.py
@@ -11,7 +11,11 @@ class cfworker(threading.Thread):
                 threading.Thread.__init__(self)
 		self.port = port
 		self.log = log
+		if not self.port:
+			# update to default CF port
+			self.port = int(os.getenv("VCAP_APP_PORT"))
 		if self.log:
+			# update from the system default
 			logging.getLogger('werkzeug').setLevel(self.log)
 		self.start()
                	app.run( host='0.0.0.0', port=self.port )
