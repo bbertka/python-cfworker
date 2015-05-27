2881953a5d299173f19466b5b6b0dbe05298c8e2
Ben Bertka <bbertka@gmail.com>
2015-05-27 10:33:50 -0700

    restructured example to be simpler

--- examples/main.py
+++ examples/main.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python
-
 import cfworker
 import time
 
-class cfworker(cfworker.cfworker):
-	""" cfworker.run must be overridden as shown here """
 
-	def run(self):
-		while True:
-                	print "cfworker: Working..."
-			time.sleep(5)
+cfworker()
+
+while True:
+	print 'working...'
+	time.sleep(5)
 
 
-if __name__=='__main__':
-
-	w = cfworker()
