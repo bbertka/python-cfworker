b4c7c2549df2f9daba25406c4fba1d2f293094ac
Ben Bertka <bbertka@gmail.com>
2015-05-27 11:13:16 -0700

    updated version 1.3

--- setup.py
+++ setup.py
@@ -2,9 +2,20 @@
 
 from distutils.core import setup
 setup(name='python-cfworker',
-      version='1.2',
+      version='1.3',
       py_modules=['cfworker'],
       install_requires=[
         "Flask",
       ],
-      )
+      url = 'https://pypi.python.org/pypi/python-cfworker',
+      author = 'Ben Bertka',
+      author_email = 'bbertka@gmail.com',
+      maintainer = 'Ben Bertka',
+      maintainer_email = 'bbertka@gmail.com',
+      download_url = 'https://github.com/bbertka/python-cfworker',
+      summary = 'CFWorker: A Module to simplify Worker App deployment',
+      license = 'Apache',
+      description = 'This module makes it easier to deploy Python workers by wrapping Flask and Threading functionality into a single module. When instanced, your app will be serving http via Flask, as well as performing work.',
+      keywords = ['cloudfoundry', 'flask', 'worker', 'python'],
+      classifiers = [],
+)
