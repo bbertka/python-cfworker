c7c4a51cca3564bdaa88a5db105de3355f55563b
Ben Bertka <bbertka@gmail.com>
2015-05-26 18:09:16 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from distutils.core import setup
-setup(name='cfworker',
+setup(name='python-cfworker',
       version='1.1',
       py_modules=['cfworker'],
       install_requires=[
