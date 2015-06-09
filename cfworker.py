7a1cc70913bec1acca8074277fdd5298d3d42327
bbertka <bbertka@gmail.com>
2015-06-09 09:39:12 -0700

    New implementation with processing

--- cfworker.py
+++ cfworker.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python
 import flask
-import threading
+import multiprocessing
 
 
-class cfworker(threading.Thread, flask.Flask):
+class cfworker(multiprocessing.Process, flask.Flask):
 
         def __init__(self, port=None):
-                threading.Thread.__init__(self)
+                self.server = multiprocessing.Process(target=self.run)
                 self.app = flask.Flask(__name__)
                 self.port = port
                 self.start()
 
         def run(self):
                 self.app.run( host='0.0.0.0', port=self.port )
+
+        def start(self):
+                self.server.start()
+
+        def stop(self):
+                self.server.terminate()
+                self.server.join()
